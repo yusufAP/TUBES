4bb22a4284935cdf69e696e98339275fd1916ee8
Yusuf Agung Purnomo <yusufagung19@gmail.com>
2017-05-04 15:46:05 +0700

    Add files via upload
    file kontak untuk ngehubungin kalau app nya bermasalah. form login file login untuk user

---
+++ contact.php
@@ -0,0 +1,23 @@
+<div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                <p>Jika aplikasi kami bermasalah harap mengubungi, dengan cara mengisi form pesan di bawah ini!</p>
+                
+                <form name="sentMessage" id="contactForm" novalidate>
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls">
+                            <label>Pesan dan Saran</label>
+                            <textarea rows="5" class="form-control" placeholder="Tuliskan Pesan dan Saran anda di sini!!!" id="message"></textarea>
+                            
+                        
+                        </div>
+                    </div>
+                    <br>
+                    <div id="success"></div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-secondary">Send</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
