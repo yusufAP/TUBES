3efb2a63166363ca14d3751681fcb1712422bec5
Yusuf Agung Purnomo <yusufagung19@gmail.com>
2017-05-04 15:44:53 +0700

    Add files via upload
    file untuk membuat akun

---
+++ buatakun.php
@@ -0,0 +1,25 @@
+<div align="right">
+	<form  id="form1" method="post" action="<?php echo site_url('buatakun/insert')?>">
+				<table>
+				<br>
+				<br>
+				<br>
+				<tr>
+				<td><input type="text" name="username" class="form-control" placeholder="Masukan username"/></td>
+				</tr>
+				<tr>
+				<td><input type="Password" name="password" class="form-control" placeholder="Masukan Password"/></td>
+				</tr>
+				<tr>
+				<td>
+					<div align="center">
+					<input name="Submit" type="submit" value="Buat User" class="btn btn-success btn-small" />
+					<input type="reset" name="reset" value="Reset" class="btn btn-warning btn-small"/>
+					</div>
+				</td>
+				</tr>
+				</table>
+				</form>
+			</div>
+		</div> 
+	</div> 
